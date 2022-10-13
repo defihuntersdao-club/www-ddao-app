0edb5d0487bd657bda8482b395286c1076489187
liksagen.com <github@defihuntersdao.club>
2022-10-13 10:11:51 +0000

    some changes

--- htdocs/inc/blk/blks.php
+++ htdocs/inc/blk/blks.php
@@ -216,7 +216,7 @@ print "
 print "	
 							<div class=\"grid-item__details\" id=\"dropdownGridItem\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">
 								<span>Detail</span>
-`								<svg width=\"12\" height=\"8\" viewBox=\"0 0 12 8\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">
+								<svg width=\"12\" height=\"8\" viewBox=\"0 0 12 8\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">
 									<path d=\"M1.16016 1.42017L6.00097 6.26099L10.8418 1.42017\" stroke=\"#3D8AFF\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/>
 									</svg>
 									
